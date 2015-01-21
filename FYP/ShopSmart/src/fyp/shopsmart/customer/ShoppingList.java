4f4bcfe68c5ed9b0ceaa59b0bb737af4d654a003
ianDervan <iandervan@ymail.com>
2015-01-21 00:46:28 +0000

    More work on database

--- FYP/ShopSmart/src/fyp/shopsmart/customer/ShoppingList.java
+++ FYP/ShopSmart/src/fyp/shopsmart/customer/ShoppingList.java
@@ -6,8 +6,12 @@ import java.util.StringTokenizer;
 import android.R.string;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import fyp.shopsmart.R;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +19,7 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,10 +27,14 @@ public class ShoppingList extends Activity {
 	
 	AutoCompleteTextView textView;
 	TextView txtMsg;
+	EditText inputQuantity;
 	Button  btnClearText;
 	Button  btnAddItem;
 	ArrayList<String> storeItem;
-	ArrayList<String> storeCost;
+	ArrayList<String> price;
+	ArrayList<String> quantity;
+	
+	SQLiteDatabase db;
 	
 	
 	
@@ -36,11 +45,13 @@ public class ShoppingList extends Activity {
 		setContentView(R.layout.activity_shopping_list);
 		
 		txtMsg = (TextView) findViewById(R.id.txtMsg);
+		inputQuantity = (EditText) findViewById(R.id.quantity);
 		btnClearText = (Button) findViewById(R.id.cleartext);
 		btnAddItem = (Button) findViewById(R.id.addItem);
 		
 		storeItem = new ArrayList<String>();
-		storeCost = new ArrayList<String>();
+		price = new ArrayList<String>();
+		quantity = new ArrayList<String>();
 
 	    textView = (AutoCompleteTextView) findViewById(R.id.autocomplete);
 		String[] suggestedItems = getResources().getStringArray(R.array.suggestions_array);
@@ -59,33 +70,26 @@ public class ShoppingList extends Activity {
 		
 		btnAddItem.setOnClickListener(new OnClickListener() {	
 	      	public void onClick(View v) {
-	      		
-			
-				
-				String storeInput =textView.getText().toString();
-						
+
+				String storeInput =textView.getText().toString();		
 				String[] inputItem = storeInput.split("€");
-				
-			
+
 					
-				
-				 txtMsg.append(inputItem[0]+"\n");
-				 txtMsg.append(inputItem[1]+"\n");
+				quantity.add(inputQuantity.getText().toString());
+
+
+				 storeItem.add(inputItem[0]);
+				 price.add("€"+inputItem[1]);
 				 
+				 for(String testp : price)
+				 {
+					 txtMsg.append(testp);
+				 }
 				 
-				
-				
-				
-				for (String item : storeItem)
-				  {
-				    txtMsg.setText(item+"\n");
-				  }
-				for (String cost : storeCost)
-				  {
-				    txtMsg.setText(cost+"\n");
-				  }
-				
-				
+				 openDatabase();
+				 dropTable();
+				 insertSomeDbData();
+
 	      		}		
 	 		});
 		
@@ -110,4 +114,71 @@ public class ShoppingList extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void openDatabase() {
+		try {
+
+			String SDcardPath = Environment.getExternalStorageDirectory().getPath();
+			String myDbPath = SDcardPath + "/" + "shopingList.db";
+		
+			db = SQLiteDatabase.openDatabase(myDbPath, null,
+					SQLiteDatabase.CREATE_IF_NECESSARY);
+
+		} catch (SQLiteException e) {
+
+			finish();
+		}
+	}
+	private void insertSomeDbData() {
+		
+		db.beginTransaction();
+		try {
+			// create table
+			db.execSQL("create table shoppingList ("
+					+ " itemID integer PRIMARY KEY autoincrement, "
+					+ " Item  text, " + " Price text, " +" Quantity text);");
+			// commit your changes
+			db.setTransactionSuccessful();
+
+			txtMsg.append("\n-insertSomeDbData - Table was created");
+
+		} catch (SQLException e1) {
+			txtMsg.append("\nError insertSomeDbData: " + e1.getMessage());
+			finish();
+		} finally {
+			db.endTransaction();
+		}
+
+		
+		db.beginTransaction();
+		try {
+			
+			for(int i = 0; i < storeItem.size(); i++){
+				db.execSQL("insert into shoppingList (Item,Price,Quantity) "
+						+ " values ('"+ storeItem.get(i)+"', '" + price.get(i)+"', '" + quantity.get(i)+"');");
+			}
+			db.setTransactionSuccessful();
+			
+			
+		} catch (SQLiteException e2) {
+			txtMsg.append("\nError insertSomeDbData: " + e2.getMessage());
+			
+		} finally {
+			db.endTransaction();
+		}
+
+	}
+
+	private void dropTable() {
+		// (clean start) action query to drop table
+
+		try {
+			db.execSQL("DROP TABLE IF EXISTS shoppingList;");
+			//txtMsg.append("\n-dropTable - dropped!!");
+		} catch (Exception e) {
+			txtMsg.append("\nError dropTable: " + e.getMessage());
+			finish();
+		}
+	}
+	
 }
