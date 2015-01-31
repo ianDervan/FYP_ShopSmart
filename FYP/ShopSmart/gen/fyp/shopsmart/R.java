6413252b12fd53b7f2169ebb14de46897220a51e
ianDervan <iandervan@ymail.com>
2015-01-31 13:57:06 +0000

    Scanner Working, Reading Xml aswell

--- FYP/ShopSmart/gen/fyp/shopsmart/R.java
+++ FYP/ShopSmart/gen/fyp/shopsmart/R.java
@@ -9,7 +9,7 @@ package fyp.shopsmart;
 
 public final class R {
     public static final class array {
-        public static final int suggestions_array=0x7f070000;
+        public static final int suggestions_array=0x7f080000;
     }
     public static final class attr {
     }
@@ -21,8 +21,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int custombutton=0x7f020000;
@@ -33,39 +33,39 @@ public final class R {
         public static final int launcher=0x7f020005;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f090014;
-        public static final int ScrollView01=0x7f090015;
-        public static final int action_settings=0x7f090020;
-        public static final int addItem=0x7f09000d;
-        public static final int autocomplete=0x7f09000a;
-        public static final int button1=0x7f090008;
-        public static final int button2=0x7f090003;
-        public static final int button3=0x7f090004;
-        public static final int button4=0x7f090005;
-        public static final int button5=0x7f090009;
-        public static final int cleartext=0x7f09000e;
-        public static final int editText1=0x7f090018;
-        public static final int editText2=0x7f09001a;
-        public static final int exit=0x7f09001d;
-        public static final int hide=0x7f09000f;
-        public static final int item=0x7f090001;
-        public static final int linearLayout1=0x7f09000c;
-        public static final int linearLayout2=0x7f090010;
-        public static final int quantity=0x7f09000b;
-        public static final int remove=0x7f09001e;
-        public static final int removeAll=0x7f090012;
-        public static final int removeOne=0x7f090011;
-        public static final int scan=0x7f090002;
-        public static final int scanBarcode=0x7f090000;
-        public static final int shoplist=0x7f090006;
-        public static final int show=0x7f09001f;
-        public static final int signIn=0x7f090007;
-        public static final int spending=0x7f090013;
-        public static final int textView1=0x7f090017;
-        public static final int textView2=0x7f090019;
-        public static final int textView3=0x7f09001b;
-        public static final int textView4=0x7f09001c;
-        public static final int txtMsg=0x7f090016;
+        public static final int LinearLayout01=0x7f0a0014;
+        public static final int ScrollView01=0x7f0a0015;
+        public static final int action_settings=0x7f0a0020;
+        public static final int addItem=0x7f0a000d;
+        public static final int autocomplete=0x7f0a000a;
+        public static final int button1=0x7f0a0008;
+        public static final int button2=0x7f0a0003;
+        public static final int button3=0x7f0a0004;
+        public static final int button4=0x7f0a0005;
+        public static final int button5=0x7f0a0009;
+        public static final int cleartext=0x7f0a000e;
+        public static final int editText1=0x7f0a0018;
+        public static final int editText2=0x7f0a001a;
+        public static final int exit=0x7f0a001d;
+        public static final int hide=0x7f0a000f;
+        public static final int item=0x7f0a0001;
+        public static final int linearLayout1=0x7f0a000c;
+        public static final int linearLayout2=0x7f0a0010;
+        public static final int quantity=0x7f0a000b;
+        public static final int remove=0x7f0a001e;
+        public static final int removeAll=0x7f0a0012;
+        public static final int removeOne=0x7f0a0011;
+        public static final int scan=0x7f0a0002;
+        public static final int scanBarcode=0x7f0a0000;
+        public static final int shoplist=0x7f0a0006;
+        public static final int show=0x7f0a001f;
+        public static final int signIn=0x7f0a0007;
+        public static final int spending=0x7f0a0013;
+        public static final int textView1=0x7f0a0017;
+        public static final int textView2=0x7f0a0019;
+        public static final int textView3=0x7f0a001b;
+        public static final int textView4=0x7f0a001c;
+        public static final int txtMsg=0x7f0a0016;
     }
     public static final class layout {
         public static final int activity_barcode_scan=0x7f030000;
@@ -76,37 +76,37 @@ public final class R {
         public static final int activity_spending=0x7f030005;
     }
     public static final class menu {
-        public static final int barcode_scan=0x7f080000;
-        public static final int employee__menu=0x7f080001;
-        public static final int employee_menu=0x7f080002;
-        public static final int main=0x7f080003;
-        public static final int shopping_list=0x7f080004;
-        public static final int sign_in=0x7f080005;
-        public static final int spending=0x7f080006;
+        public static final int barcode_scan=0x7f090000;
+        public static final int employee__menu=0x7f090001;
+        public static final int employee_menu=0x7f090002;
+        public static final int main=0x7f090003;
+        public static final int shopping_list=0x7f090004;
+        public static final int sign_in=0x7f090005;
+        public static final int spending=0x7f090006;
     }
     public static final class string {
-        public static final int action_settings=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int app_name1=0x7f040011;
-        public static final int changeprice=0x7f04000c;
-        public static final int hello_world=0x7f040001;
+        public static final int action_settings=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int app_name1=0x7f050011;
+        public static final int changeprice=0x7f05000c;
+        public static final int hello_world=0x7f050001;
         /**  Employee Menu 
          */
-        public static final int hoursworked=0x7f040007;
-        public static final int manage=0x7f040008;
-        public static final int messages=0x7f04000b;
-        public static final int password=0x7f040005;
-        public static final int rota=0x7f04000a;
-        public static final int signin=0x7f040006;
-        public static final int title_activity_barcode_scan=0x7f040010;
-        public static final int title_activity_employee_menu=0x7f04000d;
-        public static final int title_activity_shopping_list=0x7f04000e;
-        public static final int title_activity_sign_in=0x7f040003;
-        public static final int title_activity_spending=0x7f04000f;
-        public static final int updatestock=0x7f040009;
+        public static final int hoursworked=0x7f050007;
+        public static final int manage=0x7f050008;
+        public static final int messages=0x7f05000b;
+        public static final int password=0x7f050005;
+        public static final int rota=0x7f05000a;
+        public static final int signin=0x7f050006;
+        public static final int title_activity_barcode_scan=0x7f050010;
+        public static final int title_activity_employee_menu=0x7f05000d;
+        public static final int title_activity_shopping_list=0x7f05000e;
+        public static final int title_activity_sign_in=0x7f050003;
+        public static final int title_activity_spending=0x7f05000f;
+        public static final int updatestock=0x7f050009;
         /**  Sign In 
          */
-        public static final int username=0x7f040004;
+        public static final int username=0x7f050004;
     }
     public static final class style {
         /** 
@@ -130,10 +130,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int barcodes=0x7f040000;
     }
 }
--- FYP/ShopSmart/src/fyp/shopsmart/customer/BarcodeScan.java
+++ FYP/ShopSmart/src/fyp/shopsmart/customer/BarcodeScan.java
@@ -2,6 +2,10 @@ package fyp.shopsmart.customer;
 
 
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.xmlpull.v1.XmlPullParser;
 
@@ -12,7 +16,9 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.DocumentsContract.Document;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -26,13 +32,18 @@ import com.dm.zbar.android.scanner.ZBarScannerActivity;
 import fyp.shopsmart.R;
 import net.sourceforge.zbar.Symbol;
 
+
+
 public class BarcodeScan extends Activity {
 	
 	
 	Button  btnScan;
-	EditText txtItem;
+	TextView txtItem;
+	String resultBarcode;
 	
-	int i =0;
+	int n =0;
+	
+	int check =0;
     private static final int ZBAR_SCANNER_REQUEST = 0;
 
 
@@ -42,7 +53,7 @@ public class BarcodeScan extends Activity {
         setContentView(R.layout.activity_barcode_scan);
         
         btnScan = (Button) findViewById(R.id.scanBarcode);
-        txtItem = (EditText) findViewById(R.id.item);
+        txtItem = (TextView) findViewById(R.id.item);
         
         btnScan.setOnClickListener(new OnClickListener() {	
     		public void onClick(View v) {
@@ -52,15 +63,39 @@ public class BarcodeScan extends Activity {
     			 
     	        //txtItem.append("\nHOW" + i);
     			
-    			 InputStream is=getResources().openRawResource(res.values.my_xml);
+    			// InputStream is=getResources().openRawResource(res.values.my_xml);
     			
-    			Integer xmlResFile =  //res//values/suggestions.xml;
-				new backgroundAsyncTask().execute(xmlResFile);
+    		
+    			//parse using builder to get DOM representation of the XML file
+    	    	if(check==1)
+    			{
+
+    				startXml();
+    			}
+    		
     			
     		}
     	});
         
-       
+
+        
+        //startXml();
+        
+    }
+    
+    public void startXml()
+    {
+    	
+    
+    		 Toast.makeText(this, "Im in Here", Toast.LENGTH_LONG).show();
+    			//txtItem.setText("");
+				//txtItem.append (" " +resultBarcode + "\n");
+				//Integer xmlResFile =  R.xml.barcodes;
+				//new backgroundAsyncTask().execute(xmlResFile);
+	
+    	
+    	
+    	
     }
 
 
@@ -87,9 +122,12 @@ public class BarcodeScan extends Activity {
 
     	if (resultCode == RESULT_OK) {
     		//Toast.makeText(this, "Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_LONG).show();
-    		i=1;
-    		txtItem.append("Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT));
-    		txtItem.append("\n");
+    		check=1;
+    		resultBarcode = data.getStringExtra(ZBarConstants.SCAN_RESULT);
+    		txtItem.append(resultBarcode +"\n");
+    		
+    		
+    		//txtItem.append("\n");
     	} else if(resultCode == RESULT_CANCELED && data != null) {
     		String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
     		if(!TextUtils.isEmpty(error)) {
@@ -98,127 +136,123 @@ public class BarcodeScan extends Activity {
     	}
     }
 
-	public class backgroundAsyncTask extends
-			AsyncTask<Integer, Void, StringBuilder> {
-		
-		ProgressDialog dialog = new ProgressDialog(BarcodeScan.this);
+    public class backgroundAsyncTask extends
+	AsyncTask<Integer, Void, StringBuilder> {
 
-		@Override
-		protected void onPostExecute(StringBuilder result) {
-			super.onPostExecute(result);
-			dialog.dismiss();
-			txtMsg.setText(result.toString());
+ProgressDialog dialog = new ProgressDialog(BarcodeScan.this);
+
+@Override
+protected void onPostExecute(StringBuilder result) {
+	super.onPostExecute(result);
+	dialog.dismiss();
+	txtItem.append (result.toString());
+	txtItem.append( "\n" + resultBarcode);
+}
+
+@Override
+protected void onPreExecute() {
+	super.onPreExecute();
+	dialog.setMessage("Please wait...");
+	dialog.setCancelable(false);
+	dialog.show();
 	
-		}
+	
+	
+	
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			dialog.setMessage("Please wait...");
-			dialog.setCancelable(false);
-			dialog.show();
+}
 
-		}
+@Override
+protected void onProgressUpdate(Void... values) {
+	super.onProgressUpdate(values);
+}
 
-		@Override
-		protected void onProgressUpdate(Void... values) {
-			super.onProgressUpdate(values);
-		}
+@Override
+protected StringBuilder doInBackground(Integer... params) {
+	int xmlResFile = params[0];
+	XmlPullParser parser = getResources().getXml(xmlResFile);
+			
+	StringBuilder stringBuilder = new StringBuilder();
+	String nodeText = "";
+	String nodeName = "";
+	try {
+		int eventType = -1;
+		while (eventType != XmlPullParser.END_DOCUMENT) {
 
-		@Override
-		protected StringBuilder doInBackground(Integer... params) {
-			int xmlResFile = params[0];
-			XmlPullParser parser = getResources().getXml(xmlResFile);
+			eventType = parser.next();
+
+			if (eventType == XmlPullParser.START_DOCUMENT) {
+				//stringBuilder.append("\nSTART_DOCUMENT");
+
+			} else if (eventType == XmlPullParser.END_DOCUMENT) {
+				//stringBuilder.append("\nEND_DOCUMENT");
+
+			} else if (eventType == XmlPullParser.START_TAG) {
+				//nodeName = parser.getName();
+				//stringBuilder.append("\nSTART_TAG: " + nodeName);
+				
+	
+
+					stringBuilder.append(getAttributes(parser));
 					
-			StringBuilder stringBuilder = new StringBuilder();
-			String nodeText = "";
-			String nodeName = "";
-			String xmlTag = null;
-			try {
-				int eventType = -1;
-				while (eventType != XmlPullParser.END_DOCUMENT) {
 
-					eventType = parser.next();
+			} else if (eventType == XmlPullParser.END_TAG) {
+				//nodeName = parser.getName();
+				//stringBuilder.append("\nEND_TAG:   " + nodeName );
 
-					if (eventType == XmlPullParser.START_TAG) {
-						nodeName = parser.getName();
-						if(nodeName.equals( "temperature1"))
-						{
-							xmlTag="tem";
-						}
-
-
-					} else if (eventType == XmlPullParser.END_DOCUMENT) {
-						//stringBuilder.append("\nEND_DOCUMENT");
-
-					} else if (eventType == XmlPullParser.START_TAG) {
-						nodeName = parser.getName();
-						//stringBuilder.append("\nSTART_TAG: " + nodeName);
-
-						//stringBuilder.append(getAttributes(parser));
-
-					} else if (eventType == XmlPullParser.END_TAG) {
-						//nodeName = parser.getName();
-						//stringBuilder.append("\nEND_TAG:   " + nodeName );
-
-					} else if (eventType == XmlPullParser.TEXT) {
-						
-						
-							nodeText = parser.getText();
-						//	stringBuilder.append("\n  " + nodeText );
-							
-							if(xmlTag.equals("temperature1"))
-							{
-								tempValues.add(nodeText);
-							}
-							else if(xmlTag.equals("temperature2"))
-							{
-								tempValues.add(nodeText);
-								
-							}
-							else if(xmlTag.equals("temperature3"))
-							{
-								tempValues.add(nodeText);
-							}
-							else if(xmlTag.equals("temperature4"))
-							{
-								tempValues.add(nodeText);
-							}
-							else if(xmlTag.equals("temperature5"))
-							{
-								tempValues.add(nodeText);
-							}
-							
-						
-					}
-				}
-			} catch (Exception e) {
-				Log.e("<<PARSING ERROR>>", e.getMessage());
+			} else if (eventType == XmlPullParser.TEXT && n == 1) {
+				nodeText = parser.getText();
+				stringBuilder.append( nodeText);
+				
+				n =0;
 
 			}
+		}
+	} catch (Exception e) {
+		Log.e("<<PARSING ERROR>>", e.getMessage());
 
-			return stringBuilder;
-		}// doInBackground
+	}
 
-		private String getAttributes(XmlPullParser parser) {
-			StringBuilder stringBuilder = new StringBuilder();
-			// trying to detect inner attributes nested inside a node tag
-			String name = parser.getName();
-			if (name != null) {
-				int size = parser.getAttributeCount();
+	return stringBuilder;
+}// doInBackground
 
-				for (int i = 0; i < size; i++) {
-					String attrName = parser.getAttributeName(i);
-					String attrValue = parser.getAttributeValue(i);
-					stringBuilder.append("\n    Attrib <key,value>= "
-							+ attrName + ", " + attrValue);
-				}
+private String getAttributes(XmlPullParser parser) {
+	StringBuilder stringBuilder = new StringBuilder();
+	// trying to detect inner attributes nested inside a node tag
+	String name = parser.getName();
+	if (name != null) {
+		int size = parser.getAttributeCount();
 
+		for (int i = 0; i < size; i++) {
+			String attrName = parser.getAttributeName(i);
+			
+			String attrValue = parser.getAttributeValue(i);
+			//stringBuilder.append("\n    Attrib <key,value>= "
+				//	+ attrName + ", " + attrValue);
+			
+			//stringBuilder.append("\n" +result);
+			
+			if( attrValue.equals("\""+resultBarcode+"\""))
+			{
+				//stringBuilder.append("story");
+				
+			  n = 1;	
+			  //resultBarcode= null;
 			}
-			return stringBuilder.toString();
+			
+		}
 
-		}// innerElements
+	}
+	return stringBuilder.toString();
 
-	}// backroundAsyncTask
+}// innerElements
+
+}// backroundAsyncTask
+
+
+
+
+
+	
     
 }
